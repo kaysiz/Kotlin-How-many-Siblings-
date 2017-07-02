f2a5f7f9ed125c3d34d8faadc1aca643946e8fb6
kaysiz <ksiziva@gmail.com>
2017-07-03 00:46:43 +0200

    initial commit of the project

---
+++ src/howmanysiblings.kt
@@ -0,0 +1,12 @@
+/**
+ * Created by kaysiz on 7/3/17.
+ */
+fun main(args: Array<String>) {
+    /*
+        The user is prompted with a question
+        The user has to enter an answer
+        If the answer is incorrect , then the user is prompted with the question
+        Else the application terminates
+     */
+
+}
