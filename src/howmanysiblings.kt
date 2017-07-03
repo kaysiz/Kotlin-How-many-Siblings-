0a1b028991b8642a5d0995c9592529279e1e1e11
kaysiz <ksiziva@gmail.com>
2017-07-03 12:06:24 +0200

    Complete project

--- src/howmanysiblings.kt
+++ src/howmanysiblings.kt
@@ -8,5 +8,18 @@ fun main(args: Array<String>) {
         If the answer is incorrect , then the user is prompted with the question
         Else the application terminates
      */
+    var isTrue: Boolean = false
 
+    do {
+        println("How many siblings does Kuda have?")
+        var answer: Int? = readLine()!!.toInt()
+
+        when(answer){
+            12 -> {
+                println("You are correct!")
+                isTrue = true
+            }
+            else -> println("Incorrect! Try Again \n")
+        }
+    }while (isTrue == false)
 }
